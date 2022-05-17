bed5f3701e25948e92fa001543519049cde706cc
Jocyline Magero <jmagero54@gmail.com>
2022-05-17 14:37:08 -0500

    Initial commit

---
+++ src/test/java/jm/ems/emsbackend/EmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package jm.ems.emsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
