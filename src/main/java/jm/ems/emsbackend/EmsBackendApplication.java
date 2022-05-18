4c15ff3126df783f77a47b1808314e2b0434d0ac
Jocyline Magero <jmagero54@gmail.com>
2022-05-18 10:50:50 -0500

    Update EmsBackendApplication.java
    delete comments

--- src/main/java/jm/ems/emsbackend/EmsBackendApplication.java
+++ src/main/java/jm/ems/emsbackend/EmsBackendApplication.java
@@ -3,12 +3,12 @@ package jm.ems.emsbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class EmsBackendApplication {
-
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(EmsBackendApplication.class, args);
-		//this is a back end
 	}
-
+	
 }
