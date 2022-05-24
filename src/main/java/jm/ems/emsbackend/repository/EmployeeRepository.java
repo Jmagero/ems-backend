bddd748a88bf5b3888e6d589e1deb66512468aa1
Jocyline Magero <jmagero54@gmail.com>
2022-05-24 15:51:59 -0500

    Update EmployeeRepository.java
    Add save method for Optional Employee

--- src/main/java/jm/ems/emsbackend/repository/EmployeeRepository.java
+++ src/main/java/jm/ems/emsbackend/repository/EmployeeRepository.java
@@ -1,10 +1,14 @@
 package jm.ems.emsbackend.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import jm.ems.emsbackend.model.Employee;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+
+ Optional<Employee> save(Optional<Employee> employee);
 
 }
